2a773ebe3629152603e3566034497412a0950004
Тарас Курцеба <taraskurtseba@gmail.com>
2023-08-06 12:56:19 +0300

    #Added view windows

---
+++ Views/MemoryLimitWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test_Assignment.Views
+{
+    /// <summary>
+    /// Interaction logic for MemoryLimitWindow.xaml
+    /// </summary>
+    public partial class MemoryLimitWindow : Window
+    {
+        public MemoryLimitWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void CommandBinding_CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
---
+++ Views/TimeLimitWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test_Assignment.Views
+{
+    /// <summary>
+    /// Interaction logic for TimeLimitWindow.xaml
+    /// </summary>
+    public partial class TimeLimitWindow : Window
+    {
+        public TimeLimitWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
